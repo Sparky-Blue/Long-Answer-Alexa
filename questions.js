dd5d062457ecbb16b57a52befb48c3d159ca2103
Clare <clare.pickering@isotoma.com>
2018-05-29 17:26:54 +0100

    add questions including phoneme and spell out words

---
+++ questions.js
@@ -0,0 +1,176 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+  cardiovascular: [
+    {
+      "What is the most commonly involved coronary artery in myocardial infarction (<say-as interpret-as='spell-out'>MI</say-as>)?":
+        "Right coronary artery"
+    },
+
+    {
+      "Ventricular septal defect (VSD) is the most common congenital heart defect, what condition is it most likely to be associated with?":
+        "Congenital rubella"
+    },
+    {
+      "What is the most common cause of sudden cardiac death (SCD)?":
+        "Ventricular arrhythmia"
+    },
+    {
+      "What are the characteristics of stable angina?": "Bradycardia"
+    },
+
+    {
+      "What heart condition is Turner’s syndrome associated with?":
+        "Tricuspid atresia"
+    },
+    {
+      "What is the most common cause of right sided heart failure?":
+        "Hypothyroidism"
+    },
+    {
+      "What colour is the sky?": "blue"
+    }
+    // {
+    //   "What heart condition is Marfan’s syndrome associated with?": [
+    //     "Endocarditis",
+    //     "Restrictive cardiomyopathy",
+    //     "Arrhythmia",
+    //     "Aortic dissection"
+    //   ]
+    // },
+    // {
+    //   "What are the classic signs of a cardiac tamponade?": [
+    //     "Hypotension, muffled heart sounds, increased jugular venous distension (JVD)",
+    //     "Tachycardia, dyspnoea, fever",
+    //     "Bradycardia, weakness in arms, diaphoresis",
+    //     "Hypertension, palpitations, chest pain"
+    //   ]
+    // },
+    // {
+    //   "What does the ECG show in prinzmetal angina?": [
+    //     "ST segment elevation",
+    //     "ST segment depression",
+    //     "Absent P waves",
+    //     "Prolonged PR interval"
+    //   ]
+    // },
+    // {
+    //   "What is the most sensitive and specific marker for a myocardial infarction (MI)?": [
+    //     "AST",
+    //     "LDH",
+    //     "Troponin I",
+    //     "CK-MB"
+    //   ]
+    // },
+    // {
+    //   "What is the main complication of the macrophage phase (4 to 7 days) after an MI?": [
+    //     "Fibrinous pericarditis",
+    //     "Arrhythmia",
+    //     "Aneurysm / Mural thrombus / Dressler syndrome",
+    //     "Cardiac tamponade / Shunt through the ventricular wall / Mitral insufficiency"
+    //   ]
+    // },
+    // {
+    //   "What is the key complication in the first 24 hours of an MI?": [
+    //     "Fibrinous pericarditis",
+    //     "Coronary artery aneurysm",
+    //     "Arrhythmia",
+    //     "Mitral insufficiency"
+    //   ]
+    // },
+    // {
+    //   "What is the most frequent etiologic agent of acute infective endocarditis in IV drug abusers?": [
+    //     "Streptococcus viridans",
+    //     "Staphylococcus aureus",
+    //     "Staphylococcus epidermidis",
+    //     "Streptococcus bovis"
+    //   ]
+    // },
+    // {
+    //   "Choose the complete and accurate grouping of right to left shunts": [
+    //     "Atrial septal defect (ASD), Patent ductus arteriosus (PDA), Ventricular septal defect (VSD)",
+    //     "Truncus arteriosus, Transposition of great vessels, Tricuspid atresia, Tetralogy of Fallot, Total anomalous pulmonary venous return (TAPVR)",
+    //     "ASD, TAPVR, VSD",
+    //     "PDA, Truncus arteriosus, tetralogy of fallot"
+    //   ]
+    // },
+    // {
+    //   "What is the most common cause of mitral stenosis?": [
+    //     "Chronic rheumatic valve disease",
+    //     "Acute rheumatic fever",
+    //     "Congestive heart failure",
+    //     "Infective endocarditis"
+    //   ]
+    // },
+    // {
+    //   "What is the best description of Eisenmenger’s syndrome?": [
+    //     "It occurs when a right to left shunt becomes left to right due to a build-up of pressure on the left side of the heart.",
+    //     "It is due to failure of the aorticopulmonary septum to spiral.",
+    //     "It is due to lack or aorticopulmonary septum formation.",
+    //     "An initial left to right shunt becomes right to left due to increased pulmonary blood flow and eventual right ventricular hypertrophy (RVH)."
+    //   ]
+    // },
+    // {
+    //   "What gross histological change correlates with white blood cells’ (WBCs) invasion into cardiac tissue during the first week after an MI?": [
+    //     "Yellow pallor",
+    //     "Dark discolouration",
+    //     "White scar",
+    //     "Red border around yellow pallor"
+    //   ]
+    // },
+    // {
+    //   "What type of cardiomyopathy is seen in the sudden death of young athletes?": [
+    //     "Hypertrophic cardiomyopathy",
+    //     "Restrictive cardiomyopathy",
+    //     "Dilated cardiomyopathy",
+    //     "Dilated cardiomyopathy"
+    //   ]
+    // }
+  ],
+  paediatrics: [
+    {
+      "The answer is Sodium-Chloride Transporter": "Sodium-Chloride Transporter"
+    },
+    {
+      "The answer is Pneumatic Intermittent Impulse Device? ":
+        "Pneumatic Intermittent Impulse Device"
+    },
+    {
+      "The answer is firefly, try <phoneme alphabet='x-sampa' ph='lAmpI@r\\idel' >Lampyridae</phoneme>?":
+        "firefly"
+    },
+    {
+      "The answer is Left anterior descending artery?":
+        "Left anterior descending artery"
+    },
+    {
+      "The answer is Left circumflex artery?": "Left circumflex artery"
+    }
+  ],
+  orthopedics: [
+    {
+      "Genioplasties,": "Genioplasties,"
+    },
+    {
+      "The answer is Sodium-Chloride Transporter": "Sodium Chloride Transporter"
+    },
+    {
+      "The answer is Pneumatic Intermittent Impulse Device? ":
+        "Pneumatic Intermittent Impulse Device"
+    },
+    {
+      "The answer is firefly, try Lampyridae?": "firefly"
+    },
+    {
+      "The answer is Left anterior descending artery?":
+        "Left anterior descending artery"
+    },
+    {
+      "The answer is Left circumflex artery?": "Left circumflex artery"
+    }
+  ]
+};
